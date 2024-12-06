cb9135ce335bd739ccf2f683f6f57ea6fc4b29eb
christian <christian.celora@leadsbridge.com>
2024-12-06 23:06:51 +0100

    optimize solution + big refactor

--- src/day02/day_02.go
+++ src/day02/day_02.go
@@ -9,65 +9,62 @@ import (
 )
 
 func isLevelSafeWithTollerace(levels []int, tollerance int) bool {
-	var prev_level int
-	var is_increasing, is_safe bool
-	fmt.Println(levels, tollerance)
+	var prev_level, idx int
+	var is_safe bool
+
+	diffs := make([]int, len(levels)-1)
 
 	for i, level := range levels {
-		if i != 0 && level != levels[0] {
-			is_increasing = levels[0] < level
-			break
-		}
-	}
-
-	for i, level := range levels {
-		is_safe = true
 		if i != 0 {
-			if is_increasing && prev_level > level {
-				is_safe = false
-			} else if !is_increasing && prev_level < level {
-				is_safe = false
-			}
-
-			delta := int(math.Abs(float64(prev_level) - float64(level)))
-			if delta < 1 || delta > 3 {
-				is_safe = false
-			}
+			diffs[i-1] = prev_level - level
 		}
 
-		if !is_safe {
-			if tollerance > 0 {
-				fmt.Println(prev_level, level, i)
-				// exclude current level from levels
-				var is_safe_current bool
-				new_levels_1 := make([]int, len(levels))
-				copy(new_levels_1, levels)
-				if i == len(new_levels_1)-1 {
-					is_safe_current = isLevelSafeWithTollerace(new_levels_1[0:i], tollerance-1)
-				} else {
-					is_safe_current = isLevelSafeWithTollerace(append(new_levels_1[:i], new_levels_1[i+1:]...), tollerance-1)
-				}
+		prev_level = level
+	}
 
-				// exclude previous level (i - 1) from levels
-				var is_safe_prev bool
-				new_levels_2 := make([]int, len(levels))
-				copy(new_levels_2, levels)
-				if i-1 == 0 {
-					is_safe_prev = isLevelSafeWithTollerace(new_levels_2[i:], tollerance-1)
-				} else {
-					is_safe_prev = isLevelSafeWithTollerace(append(new_levels_2[:i-1], new_levels_2[i:]...), tollerance-1)
-				}
+	is_safe, idx = isDeltaSafe(diffs, 1, 3)
+	if !is_safe && tollerance > 0 {
+		is_safe_current := isLevelSafeWithTollerace(copyAndRemoveElement(levels, idx+1), tollerance-1)
+		is_safe_prev := isLevelSafeWithTollerace(copyAndRemoveElement(levels, idx), tollerance-1)
+		is_safe = is_safe_prev || is_safe_current
+	}
+	if is_safe {
+		return true
+	}
 
-				return is_safe_prev || is_safe_current
-			} else {
-				return false
-			}
-		} else {
-			prev_level = level
+	is_safe, idx = isDeltaSafe(diffs, -3, -1)
+	if !is_safe && tollerance > 0 {
+		is_safe_current := isLevelSafeWithTollerace(copyAndRemoveElement(levels, idx+1), tollerance-1)
+		is_safe_prev := isLevelSafeWithTollerace(copyAndRemoveElement(levels, idx), tollerance-1)
+		is_safe = is_safe_prev || is_safe_current
+	}
+
+	return is_safe
+}
+
+func isDeltaSafe(diffs []int, bottom int, ceil int) (bool, int) {
+	for i, d := range diffs {
+		if d < bottom || d > ceil {
+			return false, i
 		}
 	}
 
-	return true
+	return true, -1
+}
+
+// should have used generics
+func copyAndRemoveElement[T any](elements []T, index int) []T {
+	var elements_copy []T
+	elements_copy = make([]T, len(elements))
+	copy(elements_copy, elements)
+
+	if index == 0 {
+		return elements_copy[1:]
+	} else if index == len(elements)-1 {
+		return elements_copy[:index]
+	} else {
+		return append(elements_copy[:index], elements_copy[index+1:]...)
+	}
 }
 
 func isLevelSafe(levels []int) bool {
@@ -110,7 +107,7 @@ func isLevelSafe(levels []int) bool {
 func main() {
 	var safe_levels, safe_levels_tollerance int
 
-	lines := reader.ReadLines("./day02/data/input_test.txt")
+	lines := reader.ReadLines("./day02/data/input1_2.txt")
 	for _, line := range lines {
 		levels := strings.Split(line, " ")
 		levels_int := make([]int, len(levels))
@@ -125,35 +122,27 @@ func main() {
 		if isLevelSafe(levels_int) {
 			safe_levels++
 		} else {
-			var test1, test2 bool
-			test1 = false
-			test2 = false
-			// brute force...
-			for i := 0; i < len(levels_int); i++ {
-				tmp := make([]int, len(levels_int))
-				copy(tmp, levels_int)
-				if i == 0 {
-					tmp = tmp[1:]
-				} else if i == len(levels_int)-1 {
-					tmp = tmp[:i]
-				} else {
-					tmp = append(tmp[0:i], tmp[i+1:]...)
-				}
 
-				if isLevelSafe(tmp) {
-					test1 = true
-					safe_levels_tollerance++
-					break
-				}
-			}
+			// brute force...
+			// for i := 0; i < len(levels_int); i++ {
+			// 	tmp := make([]int, len(levels_int))
+			// 	copy(tmp, levels_int)
+			// 	if i == 0 {
+			// 		tmp = tmp[1:]
+			// 	} else if i == len(levels_int)-1 {
+			// 		tmp = tmp[:i]
+			// 	} else {
+			// 		tmp = append(tmp[0:i], tmp[i+1:]...)
+			// 	}
+
+			// 	if isLevelSafe(tmp) {
+			// 		safe_levels_tollerance++
+			// 		break
+			// 	}
+			// }
 
 			if isLevelSafeWithTollerace(levels_int, 1) {
-				test2 = true
 				safe_levels_tollerance++
-			}
-
-			if test1 != test2 {
-				fmt.Println(levels_int)
 			}
 		}
 	}
