010bb25708f62d8d94d52af6842b800a91d22083
christian <christian.celora@leadsbridge.com>
2024-12-11 22:43:22 +0100

    day7 part 2

--- src/day07/day_07.go
+++ src/day07/day_07.go
@@ -8,30 +8,30 @@ import (
 	"strings"
 )
 
-func canBeCombined(expected int, values []int) bool {
-	operations := [2]string{"add", "mul"}
+func canBeCombined(operations []string, expected int, values []int) bool {
 	n := int(math.Pow(float64(len(operations)), float64(len(values)-1)))
 
 	// make combinations
 	combinations := make([]string, n)
-	pad_format := fmt.Sprintf("%%0%db", len(values)-1) // convert in bit representation and left pad zeroes
+	// pad_format := fmt.Sprintf("%%0%db", len(values)-1) // convert in bit representation and left pad zeroes
+	pad_format := fmt.Sprintf("%%0%ds", len(values)-1) // left pad zeroes
 	for i := 0; i < n; i++ {
-		combinations[i] = fmt.Sprintf(pad_format, i)
+		// combinations[i] = fmt.Sprintf(pad_format, i)
+		combinations[i] = fmt.Sprintf(pad_format, strconv.FormatInt(int64(i), len(operations)))
 	}
 
-	// fmt.Println(values)
-	// fmt.Println("------")
 	for _, comb := range combinations {
 		var res, i int
 		res = values[i]
-		// fmt.Printf("%s (%d)\n", comb, len(comb))
 		for _, op := range comb {
 			i++
 			// 0 = add,  1 = mul
 			if op == '0' {
 				res += values[i]
-			} else {
+			} else if op == '1' {
 				res *= values[i]
+			} else {
+				res, _ = strconv.Atoi(fmt.Sprintf("%d%d", res, values[i]))
 			}
 		}
 
@@ -43,8 +43,11 @@ func canBeCombined(expected int, values []int) bool {
 }
 
 func main() {
-	var total_calibration int
+	var total_calibration, total_calibration_part_2 int
 	lines := reader.ReadLines("./day07/data/input1_2.txt")
+
+	part_1_operations := []string{"add", "mul"}
+	part_2_operations := []string{"add", "mul", "concat"}
 
 	for _, line := range lines {
 		line_slice := strings.Split(line, ":")
@@ -57,11 +60,15 @@ func main() {
 			values[i] = v
 		}
 
-		if canBeCombined(test_val, values) {
+		if canBeCombined(part_1_operations, test_val, values) {
 			total_calibration += test_val
 		}
 
+		if canBeCombined(part_2_operations, test_val, values) {
+			total_calibration_part_2 += test_val
+		}
 	}
 
 	fmt.Println("total calibration value is", total_calibration)
+	fmt.Println("total calibration value for part 2 is", total_calibration_part_2)
 }
